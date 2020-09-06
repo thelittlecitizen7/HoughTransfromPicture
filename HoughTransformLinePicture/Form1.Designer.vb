af3bcda48ec910557760f5ebae2f882e405b1ff6
thelittlecitizen <thelittlecitizen7@gmail.com>
2020-09-06 11:32:42 -0700

    finish part

---
+++ HoughTransformLinePicture/Form1.Designer.vb
@@ -0,0 +1,101 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(507, 38)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(223, 92)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "BrowsePicture"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Location = New System.Drawing.Point(34, 230)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(374, 312)
+        Me.PictureBox1.TabIndex = 1
+        Me.PictureBox1.TabStop = False
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.Location = New System.Drawing.Point(414, 230)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(374, 312)
+        Me.PictureBox2.TabIndex = 2
+        Me.PictureBox2.TabStop = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(146, 181)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(75, 13)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "OriginalPicture"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(529, 181)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(72, 13)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "AfterChanged"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 576)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+End Class
---
+++ HoughTransformLinePicture/Form1.vb
@@ -0,0 +1,55 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim picturePath As String
+        Try
+            Dim openFileDialog As New OpenFileDialog
+
+            If (openFileDialog.ShowDialog() = DialogResult.OK) Then
+                picturePath = openFileDialog.FileName
+                CreateLineOnPicture(picturePath)
+
+            End If
+        Catch ex As Exception
+            MessageBox.Show("An Error Occured with : " + ex.Message.ToString())
+        End Try
+
+    End Sub
+
+
+    Private Sub CreateCircleOnPicture(myBitmap As Bitmap)
+        Dim p As New System.Drawing.Pen(Color.Yellow, 4)
+        Dim g As System.Drawing.Graphics
+        PictureBox1.Refresh()
+        g = PictureBox1.CreateGraphics
+        g.DrawEllipse(p, 70, 10, 100, 150)
+
+        PictureBox2.Image = myBitmap
+    End Sub
+
+
+    Private Sub CreateLineOnPicture(picturePath As String)
+        PictureBox1.ImageLocation = picturePath
+
+        Dim myBitmap As New Bitmap(picturePath)
+
+        MsgBox(myBitmap.GetPixel(0, 0).ToString)
+
+
+        For y As Integer = 0 To myBitmap.Height - 1
+            For x As Integer = 0 To myBitmap.Width - 1
+
+                Dim clr As Color = myBitmap.GetPixel(x, y)
+
+                If (x > 150) And (x < 200) Then
+                    clr = Color.FromName("Red")
+
+                    myBitmap.SetPixel(x, y, clr)
+                End If
+            Next x
+        Next
+
+
+        CreateCircleOnPicture(myBitmap)
+    End Sub
+
+End Class
